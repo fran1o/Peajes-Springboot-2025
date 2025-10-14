03c7e6bf81e94a4d3e53158f7c5cd9e54c973960
fran1o <pankha994@gmail.com>
2025-10-14 19:09:12 -0300

    actualizando rama con logout en admin

---
+++ peajes/src/main/java/uy/edu/ort/obligatorio/peajes/controladores/ControladorMenuAdmin.java
@@ -0,0 +1,43 @@
+package uy.edu.ort.obligatorio.peajes.controladores;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import jakarta.servlet.http.HttpSession;
+import uy.edu.ort.obligatorio.peajes.dominio.Usuario;
+import uy.edu.ort.obligatorio.peajes.utils.Respuesta;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/menuAdmin")
+public class ControladorMenuAdmin {
+    
+    @PostMapping("/vistaConectada")
+    public List<Respuesta> inicializarVista(@SessionAttribute (name = "usuarioLogueado", required = false) Usuario usuarioLogueado) {
+        if(usuarioLogueado == null) {
+            return Respuesta.lista(new Respuesta("usuarioNoConectado", "loginAdmin.html"));
+            
+        } 
+        
+        return Respuesta.lista(new Respuesta("vistaConectada", usuarioLogueado.getNombreCompleto()));
+        
+        
+    }
+
+    @PostMapping("/logout")
+    public List<Respuesta> logout(HttpSession sessionHttp) {
+        Usuario usuario = (Usuario) sessionHttp.getAttribute("usuarioLogueado");
+        if(usuario != null) {
+            sessionHttp.removeAttribute("usuarioLogueado");
+            sessionHttp.invalidate();
+        }
+        return Respuesta.lista(new Respuesta("usuarioNoConectado", "loginAdmin.html"));
+    }
+
+    
+}
